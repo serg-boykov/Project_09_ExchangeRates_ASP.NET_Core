dc58a009181ed375396e0f07ed29609fad04c825
Sergey Boykov <boykov.sergei@gmail.com>
2022-05-23 14:43:58 +0500

    Refactoring.

--- CurrencyApp/Models/CurrencyConverter.cs
+++ CurrencyApp/Models/CurrencyConverter.cs
@@ -1,15 +1,17 @@
-﻿namespace CurrencyApp.Models
+﻿using System;
+
+namespace CurrencyApp.Models
 {
     public class CurrencyConverter
     {
         public decimal USD { get; set; }
-        public decimal ConvertToUSD(decimal priceRUB) => priceRUB / USD;
+        public decimal ConvertToUSD(decimal priceRUB) => Decimal.Round(priceRUB / USD, 2);
 
         public decimal EUR { get; set; }
-        public decimal ConvertToEUR(decimal priceRUB) => priceRUB / EUR;
+        public decimal ConvertToEUR(decimal priceRUB) => Decimal.Round(priceRUB / EUR, 2);
 
         // Nominal for KZT = 100 (According to the Central Bank of Russia)
         public decimal KZT { get; set; }
-        public decimal ConvertToKZT(decimal priceRUB) => priceRUB / (KZT / 100);
+        public decimal ConvertToKZT(decimal priceRUB) => Decimal.Round(priceRUB / (KZT / 100), 2);
     }
 }
